f90803cc08a50ce5232fc90d79a1c673b3b90329
Jay MOULIN <jaymoulin@gmail.com>
2016-11-15 00:23:40 +0100

    Add CsvHandler

---
+++ src/Monolog/Handler/CsvHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FemtoPixel\Monolog\Handler;
+
+use Monolog\Formatter\NormalizerFormatter;
+use Monolog\Handler\StreamHandler;
+
+/**
+ * Stores to a csv file
+ *
+ * Can be used to store big loads to physical files and import them later into another system that can handle CSV
+ *
+ * @author Jay MOULIN <jaymoulin@gmail.com>
+ */
+class CsvHandler extends StreamHandler
+{
+    const DELIMITER = ',';
+    const ENCLOSURE = '\'';
+    const ESCAPE_CHAR = '\\';
+
+    /**
+     * @inheritdoc
+     */
+    protected function streamWrite($resource, $record)
+    {
+        return fputcsv($resource, (array)$record['formatted'], static::DELIMITER, static::ENCLOSURE, static::ESCAPE_CHAR);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getDefaultFormatter()
+    {
+        return new NormalizerFormatter();
+    }
+}
